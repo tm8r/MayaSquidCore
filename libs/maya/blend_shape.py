9d1f6e296644acd4c1d02052e959c6f117d5d48b
tm8r <tm8r.nrm@gmail.com>
2019-03-16 15:28:35 +0900

    feat: add libraries

--- libs/maya/blend_shape.py
+++ libs/maya/blend_shape.py
@@ -16,24 +16,21 @@ def get_all_blend_shapes():
     return cmds.ls(type="blendShape")
 
 
-def get_input_targets(blend_shapes):
+def get_input_targets(blend_shape):
     u"""指定blendShapeのinputTargetを返す
 
     Args:
-        blend_shapes (list of unicode): blendShapeのリスト
+        blend_shape (unicode): blendShape
 
     Returns:
         list of unicode: inputTargetのリスト
     """
-    res = []
-    if not blend_shapes:
-        return res
-    for blend in blend_shapes:
-        input_targets = cmds.listConnections(attribute.convert_attribute(blend, attribute.INPUT_TARGET))
-        if not input_targets:
-            continue
-        res.extend(input_targets)
-    return res
+    if not blend_shape:
+        return []
+    input_targets = cmds.listConnections(attribute.convert_attribute(blend_shape, attribute.INPUT_TARGET))
+    if not input_targets:
+        return []
+    return input_targets
 
 
 def get_weight_members(blend_shape):
@@ -74,70 +71,7 @@ def get_relative_blend_shapes(target, all_descendents=False, include_self=False)
         transforms.append(target)
     for t in transforms:
         tmp_blend_shapes = cmds.ls(cmds.listHistory(t), type="blendShape", long=True)
-        if not tmp_blend_shapes or len(tmp_blend_shapes) != 1:
+        if not tmp_blend_shapes:
             continue
-        blend_shapes.append(tmp_blend_shapes[0])
+        blend_shapes.extend(tmp_blend_shapes)
     return blend_shapes
-
-
-def delete_relative_blend_shape_input_targets(target, all_descendents=False, include_self=False):
-    u"""指定ノード配下のblendShapeのinputTargetを削除する
-
-    Args:
-        target (unicode): 対象のノード
-        all_descendents (bool): 孫も対象とするかどうか
-        include_self (bool): 自身も対象とするかどうか
-    """
-    blend_shapes = get_relative_blend_shapes(target,
-                                             all_descendents=all_descendents,
-                                             include_self=include_self)
-    if not blend_shapes:
-        return
-    input_targets = get_input_targets(blend_shapes)
-    if not input_targets:
-        return
-    cmds.delete(input_targets)
-
-
-def collect_relative_post_deformation_node(target, all_descendents=False, include_self=False):
-    u"""指定ノード配下のpost-deformationになっているノードを収集
-
-    Args:
-        target (unicode): 対象のノード
-        all_descendents (bool): 孫も対象とするかどうか
-        include_self (bool): 自身も対象とするかどうか
-
-    Returns:
-        list of unicode: post-deformationになっているノードのリスト
-
-    """
-    return collect_post_deformation_node(get_relative_blend_shapes(target,
-                                                                   all_descendents=all_descendents,
-                                                                   include_self=include_self))
-
-
-def collect_post_deformation_node(targets=None):
-    u"""post-deformationになっているノードを収集
-
-    Args:
-        targets (list of unicode): 対象のblendShapeのリスト（指定されていなければシーンの全てのblendShape）
-
-    Returns:
-        list of unicode: post-deformationになっているノードのリスト
-    """
-    result = []
-    if targets is None:
-        targets = cmds.ls(type="blendShape")
-    for b in targets:
-        target = cmds.blendShape(b, q=True, g=True)[0]
-        histories = cmds.listHistory(target, gl=True, pdo=True, lf=True, f=False, il=2)
-        skin_cluster_found = False
-        for h in histories:
-            object_type = cmds.objectType(h)
-            if object_type == "skinCluster":
-                skin_cluster_found = True
-                continue
-            if object_type == "blendShape" and not skin_cluster_found:
-                result.append(target)
-                break
-    return result
